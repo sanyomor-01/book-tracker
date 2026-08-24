6d75733063e2cd2a144df790dc6795a2456db1ff
Michael Sanyomor <sanyomormichael@gmail.com>
2026-08-24 03:47:28 +0000

    feat: live search and isbn validation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.book-container')
 const emptyEl = document.querySelector('.empty')
 const errormsgEl = document.querySelector('.errormsg')
+const searchInput = document.querySelector('.search-input')
+
 
 // get the button and add a click event to get book data
 document.querySelector('button').addEventListener('click', getFetch)
@@ -24,13 +26,19 @@ if (storedBooks) {
 
 function getFetch() {
   //getting isbn from user
-  const isbn = document.querySelector('input').value.trim()
+  const isbn = document.querySelector('input').value.trim().replace(/-/g, '')
 
-  //edge case - empty isbn
-  if (!isbn) {
+  //edge case -  isbn validation
+  if (!isbn || (isbn.length !== 10 && isbn.length !== 13)) {
     errormsgEl.textContent = "Enter a valid isbn"
     return
   }
+
+  if ((isbn.length === 13 && !/^\d+$/.test(isbn)) || (isbn.length === 10 && !/^\d{9}[\dx]$/i.test(isbn))) {
+    errormsgEl.textContent = "Enter a valid isbn"
+    return
+  }
+
   //edge case - repeated button clicks
   const getButton = document.querySelector('button')
   getButton.disabled = true
@@ -74,18 +82,24 @@ function getFetch() {
 }
 
 //rendering books
-function renderBooks(booksArray) {
+function renderBooks(booksToRender) {
   // clear existing html element
   container.innerHTML = ""
 
-  if (booksArray.length === 0) {
+
+  if (booksToRender.length === 0) {
     emptyEl.style.display = 'block'
   } else {
     emptyEl.style.display = 'none'
 
-    booksArray.forEach(book => {
+    booksToRender.forEach(book => {
       const card = document.createElement('div')
       card.classList.add('bookCard')
+
+      const removeBtn = document.createElement('button')
+      removeBtn.dataset.isbn = book.isbn
+      removeBtn.classList.add('removeBtn')
+      removeBtn.textContent = 'Remove'
 
       const titleEl = document.createElement('h2')
       titleEl.classList.add('title')
@@ -95,12 +109,33 @@ function renderBooks(booksArray) {
       isbnEl.classList.add('isbn')
       isbnEl.textContent = book.isbn
 
+      //removing a book card
+      removeBtn.addEventListener('click', removeBookCard)
+
+      function removeBookCard(event) {
+        const btnIsbn = event.target.dataset.isbn
+        booksArray = booksArray.filter(book => book.isbn !== btnIsbn)
+        localStorage.setItem('books', JSON.stringify(booksArray))
+        renderBooks(booksArray)
+
+      }
+
+      //appending child elements 
       card.appendChild(titleEl)
       card.appendChild(isbnEl)
+      card.appendChild(removeBtn)
       container.appendChild(card)
     });
 
   }
 }
-//code to handle states
+
+//searching
+searchInput.addEventListener('input', filterBook)
+function filterBook() {
+  const inputs = searchInput.value.trim().toLowerCase()
+  const foundBook = booksArray.filter(book => book.title.toLowerCase().includes(inputs))
+  renderBooks(foundBook)
+}
+
 
